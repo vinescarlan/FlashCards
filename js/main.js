32d720b66f9f448bd6a40bced7fcac1bb7483711
vinescarlan <vinescarlan@gmail.com>
2015-09-30 15:54:37 +0800

    Create array of QAs

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var questions = [
+  Q1 = {
+    question: "What do HTML means?",
+    answer: "HyperText Markup Language"
+  },
+  Q2 = {
+    question: "What do CSS means?",
+    answer: "Cascading Style Sheet"
+  }
+];
+
