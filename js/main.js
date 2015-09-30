e2235a00a2a5e375b1b2bacbf190bdc328445bd9
vinescarlan <vinescarlan@gmail.com>
2015-09-30 16:34:22 +0800

    Increase indention by 2 spaces

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var questions = [
   Q1 = {
-    question: "What does HTML means?",
-    answer: "HyperText Markup Language"
+		question: "What does HTML means?",
+		answer: "HyperText Markup Language"
   },
   Q2 = {
-    question: "What does CSS means?",
-    answer: "Cascading Style Sheet"
+		question: "What does CSS means?",
+		answer: "Cascading Style Sheet"
   },
   Q3 = {
-    question: "Why the \"C\" in CSS, is called Cascading?",
-    answer: "When CSS rules are duplicated, the rule to be use is chosen by <em>cascading</em> down from more general rules to the specific rule required"
+		question: "Why the \"C\" in CSS, is called Cascading?",
+		answer: "When CSS rules are duplicated, the rule to be use is chosen by <em>cascading</em> down from more general rules to the specific rule required"
   }
 ];
 
@@ -19,10 +19,9 @@ var questionNum = 0;
 
 // Generate random number to display random QA set
 function createNum() {
-  questionNum = Math.floor(Math.random() * 3);
+	questionNum = Math.floor(Math.random() * 3);
 }
 
 createNum();
 question.innerHTML = questions[questionNum].question;
-
 
