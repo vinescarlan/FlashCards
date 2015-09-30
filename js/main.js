bd2ee45b9fad33587764f03dbeb6f39c86299ea6
vinescarlan <vinescarlan@gmail.com>
2015-09-30 16:32:11 +0800

    Modify and add another question

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 var questions = [
   Q1 = {
-    question: "What do HTML means?",
+    question: "What does HTML means?",
     answer: "HyperText Markup Language"
   },
   Q2 = {
-    question: "What do CSS means?",
+    question: "What does CSS means?",
     answer: "Cascading Style Sheet"
+  },
+  Q3 = {
+    question: "Why the \"C\" in CSS, is called Cascading?",
+    answer: "When CSS rules are duplicated, the rule to be use is chosen by <em>cascading</em> down from more general rules to the specific rule required"
   }
 ];
 
@@ -14,7 +18,7 @@ var question = document.getElementById('question');
 var questionNum = 0;
 
 function createNum() {
-  questionNum = Math.floor(Math.random() * 2);
+  questionNum = Math.floor(Math.random() * 3);
 }
 
 createNum();
