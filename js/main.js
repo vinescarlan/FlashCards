6e20f3b5b33dd2ce67817e6bcfbdfb0ba042aef1
vinescarlan <vinescarlan@gmail.com>
2015-10-01 11:29:03 +0800

    Remove array of questions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-var questions = [
-  Q1 = {
-		question: "What does HTML means?",
-		answer: "HyperText Markup Language"
-  },
-  Q2 = {
-		question: "What does CSS means?",
-		answer: "Cascading Style Sheet"
-  },
-  Q3 = {
-		question: "Why the \"C\" in CSS, is called Cascading?",
-		answer: "When CSS rules are duplicated, the rule to be use is chosen by <em>cascading</em> down from more general rules to the specific rule required"
-  }
-];
-
 var question = document.getElementById('question');
 
 var questionNum = 0,
